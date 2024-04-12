e001e589f900d64e6a4ec08e95f075114820e32a
Ibrohim <imirxoshimov@gmail.com>
2024-04-13 01:22:45 +0500

    Revert "parse form data"
    This reverts commit 338c0c67a7736e52076bf14ef306981bf32a576e.

--- app/api/routes/webhook.js
+++ app/api/routes/webhook.js
@@ -1,14 +1,11 @@
 const { Router } = require("express");
 const { ESKIZ_WEBHOOK_ROUTE } = require("../../constants/mix");
-const multer = require("multer");
-const upload = multer();
 
 module.exports = () => {
 	const route = Router();
 
-	route.post(ESKIZ_WEBHOOK_ROUTE, upload.none(), async (req, res) => {
+	route.post(ESKIZ_WEBHOOK_ROUTE, async (req, res, next) => {
 		console.log("ESKIZ", req.body);
-		res.send().status(200);
 	});
 
 	return route;
