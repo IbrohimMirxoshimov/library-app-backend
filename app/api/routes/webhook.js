338c0c67a7736e52076bf14ef306981bf32a576e
Ibrohim <imirxoshimov@gmail.com>
2024-04-13 01:19:38 +0500

    parse form data

--- app/api/routes/webhook.js
+++ app/api/routes/webhook.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
 const { ESKIZ_WEBHOOK_ROUTE } = require("../../constants/mix");
+const multer = require("multer");
+const upload = multer();
 
 module.exports = () => {
 	const route = Router();
 
-	route.post(ESKIZ_WEBHOOK_ROUTE, async (req, res, next) => {
+	route.post(ESKIZ_WEBHOOK_ROUTE, upload.none(), async (req, res) => {
 		console.log("ESKIZ", req.body);
+		res.send().status(200);
 	});
 
 	return route;
