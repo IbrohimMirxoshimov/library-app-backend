3c7392c1a79d6d6225562fe31a3d9f574497f44d
Ibrohim <imirxoshimov@gmail.com>
2024-04-13 11:25:06 +0500

    eskiz wh log

--- app/api/routes/webhook.js
+++ app/api/routes/webhook.js
@@ -4,8 +4,9 @@ const { ESKIZ_WEBHOOK_ROUTE } = require("../../constants/mix");
 module.exports = () => {
 	const route = Router();
 
-	route.post(ESKIZ_WEBHOOK_ROUTE, async (req, res, next) => {
-		console.log("ESKIZ", req.body);
+	route.post(ESKIZ_WEBHOOK_ROUTE, async (req, res) => {
+		console.log("ESKIZ", req.body, req.query, req.params, req.path);
+		res.send().status(200);
 	});
 
 	return route;
