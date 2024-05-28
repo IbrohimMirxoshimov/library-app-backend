4236a0d900a13db252a2c4e803d6b62fba161621
Anvar571 <abdurashidovanvar52@gmail.com>
2024-05-28 14:49:35 +0500

    feat: user create

--- src/domain/location/errors/error.messages.ts
+++ src/domain/location/errors/error.messages.ts
@@ -3,13 +3,13 @@ import { LocationErrorCodes } from "./error.codes";
 
 export const LocationErrorMessages: Record<LocationErrorCodes, MultiLanguage> = {
   [LocationErrorCodes.AlreadyExists]: {
-    [Language.English]: '',
+    [Language.English]: 'location already exists',
     [Language.Russian]: '',
-    [Language.Uzbek]: '',
+    [Language.Uzbek]: 'lokatsiya mavjud',
   },
   [LocationErrorCodes.NotFound]: {
-    [Language.English]: '',
+    [Language.English]: 'location not found',
     [Language.Russian]: '',
-    [Language.Uzbek]: '',
+    [Language.Uzbek]: 'lokatsiya topilmdi',
   }
 }
--- src/domain/location/location.ts
+++ src/domain/location/location.ts
@@ -1,4 +1,24 @@
 import { BaseEntity } from "../common/base-entity";
 import { LocationModule } from "./service";
 
-export class Location extends BaseEntity<LocationModule> {}
+export class Location extends BaseEntity<LocationModule> {
+  public get name(): string {
+    return this.data.name;
+  }
+
+  public get region_id(): number {
+    return this.data.region_id;
+  }
+
+  public get created_at(): Date {
+    return this.data.created_at;
+  }
+
+  public get updated_at(): Date {
+    return this.data.updated_at;
+  }
+
+  public set updated_at(updated_at: Date) {
+    this.data.updated_at = updated_at;
+  }
+}
