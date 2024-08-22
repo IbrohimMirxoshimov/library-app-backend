3291140accf2471c8917e633f96ec00e4b19aec3
Ibrohim <imirxoshimov@gmail.com>
2024-08-22 21:09:56 +0500

    date utils

--- app/utils/date.js
+++ app/utils/date.js
@@ -13,8 +13,71 @@ function getOneDayBackDate(date = new Date(), day = 1) {
 	return date;
 }
 
+function getLastDateOfMonth(date = new Date()) {
+	return getEndOfDay(new Date(date.getFullYear(), date.getMonth() + 1, 0));
+}
+
+function getFirstDateOfMonth(date = new Date()) {
+	return getStartOfDay(new Date(date.getFullYear(), date.getMonth(), 1));
+}
+
+function getPreviousMonth(date = new Date()) {
+	date.setMonth(date.getMonth() - 1); // Subtract one month
+	return date;
+}
+
+function getStartOfDay(date = new Date()) {
+	date.setHours(0);
+	date.setMinutes(0);
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+
+	return date;
+}
+
+function getEndOfDay(date = new Date()) {
+	date.setHours(23);
+	date.setMinutes(59);
+	date.setSeconds(59);
+	date.setMilliseconds(0);
+	return date;
+}
+
+function getFirstAndLastDateOfMonth(date = new Date()) {
+	return {
+		first: getFirstDateOfMonth(date),
+		last: getLastDateOfMonth(date),
+	};
+}
+
+const month = [
+	"Yanvar",
+	"Fevral",
+	"Mart",
+	"Aprel",
+	"May",
+	"Iyun",
+	"Iyul",
+	"Avgust",
+	"Sentabr",
+	"Oktabr",
+	"Noyabr",
+	"Dekabr",
+];
+
+function getDateMonthInUzbek(date = new Date()) {
+	return month[date.getMonth()];
+}
+
 module.exports = {
 	getOneMonthBackDate: getOneMonthBackDate,
 	getOneWeekBackDate: getOneWeekBackDate,
 	getOneDayBackDate: getOneDayBackDate,
+	getEndOfDay,
+	getStartOfDay,
+	getPreviousMonth,
+	getLastDateOfMonth,
+	getFirstDateOfMonth,
+	getFirstAndLastDateOfMonth,
+	getDateMonthInUzbek,
 };
