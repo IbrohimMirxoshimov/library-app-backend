beacd687c7f5f0bac57c2193d8cc0ca027709b5f
Ibrohim <imirxoshimov@gmail.com>
2025-09-22 11:56:30 +0500

    feat: increase SMS retrieval limit to 300 for enhanced performance

--- app/api/routes/sms.js
+++ app/api/routes/sms.js
@@ -192,7 +192,7 @@ module.exports = (app) => {
 		try {
 			const { count, rows } = await Sms.findAndCountAll(
 				getListOptions(
-					{ ...req.query, sort: "id", order: "ASC", size: 200 },
+					{ ...req.query, sort: "id", order: "ASC", size: 300 },
 					null,
 					Sms,
 					null,
